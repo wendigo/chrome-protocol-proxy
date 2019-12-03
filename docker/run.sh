70c631e6661a89428db5833b0ddb9b2a0d79e255
Mateusz Gajewski <mateusz.gajewski@gmail.com>
2019-12-03 12:43:17 +0100

    Color output

--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+ENV TERM xterm-256color
 
 chromium-browser --headless --disable-gpu --disable-software-rasterizer --disable-dev-shm-usage --no-sandbox --remote-debugging-address=0.0.0.0 --remote-debugging-port=9223 --no-sandbox &
 
