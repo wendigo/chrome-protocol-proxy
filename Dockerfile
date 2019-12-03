cacd07d944737d8b50e301bcdf11aae2eadfc1bf
Mateusz Gajewski <mateusz.gajewski@gmail.com>
2019-12-03 12:48:36 +0100

    Fix docker

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM zenika/alpine-chrome:latest
+ENV TERM xterm-256color
 COPY chrome-protocol-proxy chrome-protocol-proxy
 COPY docker/run.sh run.sh
 EXPOSE 9222 9223
--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-ENV TERM xterm-256color
-
 chromium-browser --headless --disable-gpu --disable-software-rasterizer --disable-dev-shm-usage --no-sandbox --remote-debugging-address=0.0.0.0 --remote-debugging-port=9223 --no-sandbox &
 
 ./chrome-protocol-proxy -l 0.0.0.0:9222 -r localhost:9223 "$@"
